d8099289bd798a794adb7f51d3595de8683490e8
OforiAlexander <alexanderofori07@gmail.com>
2026-06-26 16:38:36 +0100

    SCRUM-7 Added container to the logo image

--- src/client/common/assets/BrandLogo.tsx
+++ src/client/common/assets/BrandLogo.tsx
@@ -10,13 +10,16 @@ interface Props {
 
 const HEIGHTS: Record<Size, number> = { sm: 28, md: 95, lg: 48 };
 
-const BrandLogo: React.FC<Props> = ({ size = 'md' }) => (
-  <img
+const BrandLogo: React.FC<Props> = ({ size = 'sm' }) => (
+  <div className="brand-logo" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '15px', maxWidth: '100%' }}>
+    <img
     src={ebsLogo}
     alt="Elegance by Sconia"
     height={HEIGHTS[size]}
     style={{ display: 'block' }}
   />
+  </div>
+  
 );
 
 export default BrandLogo;
