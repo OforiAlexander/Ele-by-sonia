6a46b91af831b4128223af31d98ca830ea5627a7
OforiAlexander <alexanderofori07@gmail.com>
2026-06-29 20:33:51 +0100

    SCRUM-13 mailing cr3edentials issues resolution, added false fallon for mail port

--- src/server/services/mail/send-mail.ts
+++ src/server/services/mail/send-mail.ts
@@ -2,10 +2,12 @@ import nodemailer from 'nodemailer';
 import Message from '../../models/Message';
 import logger from '../logger';
 
+const mailPort = Number(process.env.MAIL_PORT ?? 587);
+
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
-  port: Number(process.env.MAIL_PORT),
-  secure: false,
+  port: mailPort,
+  secure: mailPort === 465,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASSWORD,
